9043f3e02dff21baa89194d9e047044dcaf124f8
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-10-29 15:21:56 +0300

    Fix 'Client is buggy' when strings contains double quotes

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 val commonSettings = Seq(
   organization := "com.github.fomkin",
-  version := "0.2.0",
+  version := "0.2.1",
   scalaVersion := "2.11.7",
   scalacOptions ++= Seq(
     "-deprecation",
--- scala-reql-core/src/main/scala/reql/dsl/ReqlTypesConversions.scala
+++ scala-reql-core/src/main/scala/reql/dsl/ReqlTypesConversions.scala
@@ -1,12 +1,39 @@
 package reql.dsl
 
+import scala.annotation.switch
 import scala.language.implicitConversions
 
 trait ReqlTypesConversions {
 
+  private def escape(sb: StringBuilder, s: String, unicode: Boolean): Unit = {
+    sb.append('"')
+    var i = 0
+    val len = s.length
+    while (i < len) {
+      (s.charAt(i): @switch) match {
+        case '"' => sb.append("\\\"")
+        case '\\' => sb.append("\\\\")
+        case '\b' => sb.append("\\b")
+        case '\f' => sb.append("\\f")
+        case '\n' => sb.append("\\n")
+        case '\r' => sb.append("\\r")
+        case '\t' => sb.append("\\t")
+        case c =>
+          if (c < ' ' || (c > '~' && unicode)) sb.append("\\u%04x" format c.toInt)
+          else sb.append(c)
+      }
+      i += 1
+    }
+    sb.append('"')
+  }
+
   implicit def toStr(value: String): types.Str = new types.Str {
     override def toString = value
-    val json = s""""$value""""
+    val json = {
+      val sb = new StringBuilder()
+      escape(sb, value, unicode = true)
+      sb.mkString
+    }
   }
 
   implicit def toNum(value: Int): types.Num = new types.Num {
